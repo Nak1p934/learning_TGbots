df817dd06ab9ec395c50a0c46297400a06c67fe4
Nak1p934 <leonleon0308@gmail.com>
2026-01-28 18:57:28 +0300

    add comments

--- lesson1/routes_for_lesson1.py
+++ lesson1/routes_for_lesson1.py
@@ -1,8 +1,11 @@
-from aiogram import Router
-from aiogram.types import Message
-router_less1 = Router()
+from aiogram import Router # Импортируем роутер
+from aiogram.types import Message # Импортируем тим данных Message
+router_less1 = Router() # Создаём переменную с роутером
 
 
-@router_less1.message()
-async def echo(message: Message):
-    await message.answer(f"Ваше сообщение: {message.text}")
+# Если бы роутер что то обрабатывал то в скобках что то было, например CommandStart()
+@router_less1.message() # Наш первый роутер который обрабатывает всё
+async def echo(message: Message): # Ассинхронная функция которая принимает сообщение
+    await message.answer(f"Ваше сообщение: {message.text}") # Ответ от Бота пользователю через message.answer туда мы передаём строку
+# Через f строку мы подставляем сообщение пользователя ^ с помощью message.text
+# Happy pythoning!
--- lesson1/run.py
+++ lesson1/run.py
@@ -1,18 +1,20 @@
-from aiogram import Dispatcher, Bot
-import asyncio
-from routes_for_lesson1 import router_less1
-from BOTTOKEN_LESS1 import TOKEN1
+from aiogram import Dispatcher, Bot # Импортируем нашего бота и диспетчер
+import asyncio # Импортируем asyncio
+from routes_for_lesson1 import router_less1 # Имортируем переменну нашего роутера из routes.py
+from BOTTOKEN_LESS1 import TOKEN1 # Токен нашего бота
 
 
-async def main_less1():
-    dp_less1 = Dispatcher()
-    bot_less1 = Bot(token=TOKEN1)
+async def main_less1(): # Наша основная функция
+    dp_less1 = Dispatcher() # Переменная с диспетчером
+    bot_less1 = Bot(token=TOKEN1) # Переменная с ботом
     dp_less1.include_router(router_less1)
-    await dp_less1.start_polling(bot_less1)
+    await dp_less1.start_polling(bot_less1) # Начало работы
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": # Проверка на то не импортируется файл 
     try:
         asyncio.run(main_less1()) 
     except:
-        print("Завершение работы")
+        print("Завершение работы") # Сообзение о том что бот завершил работу 
+# Завершить работу можно с помощью Ctrl + C
+# У меня файлы называются по другому что бы отличать уроки
