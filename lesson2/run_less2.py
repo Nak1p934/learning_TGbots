978fcea6ffdd9d4b6c679ed0d2bda9e4a385b6e6
Nak1p934 <leonleon0308@gmail.com>
2026-01-29 13:25:07 +0300

    bug fix router less2

--- lesson2/run_less2.py
+++ lesson2/run_less2.py
@@ -1,12 +1,12 @@
 from aiogram import Dispatcher, Bot
 import asyncio
 from BOTTOKENLESS2 import TOKEN_LESS2
-
+from routes_lesson2 import router_less2
 
 async def main_less2():
     dp_less2 = Dispatcher()
     bot_less2 = Bot()
-    dp_less2.include_router(router)
+    dp_less2.include_router(router_less2)
     await dp_less2.start_polling(bot_less2)
 
 
