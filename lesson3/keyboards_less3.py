6a83a0f5fc8c5708383090f813bd21b0a18f4344
Nak1p934 <leonleon0308@gmail.com>
2026-02-03 14:09:58 +0300

    texting function kb

--- lesson3/keyboards_less3.py
+++ lesson3/keyboards_less3.py
@@ -2,4 +2,4 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 main_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Да"), KeyboardButton(text="Нет")]
-], resize_keyboard=True)
+], resize_keyboard=True, input_field_placeholder="Ответь на вопрос кнопками", one_time_keyboard=True)
