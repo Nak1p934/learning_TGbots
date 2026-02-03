d277a321863edbefde4b35bf408b396a02e54480
Nak1p934 <leonleon0308@gmail.com>
2026-02-03 14:07:51 +0300

    resize_keyboard=True

--- lesson3/keyboards_less3.py
+++ lesson3/keyboards_less3.py
@@ -2,4 +2,4 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 main_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Да"), KeyboardButton(text="Нет")]
-])
+], resize_keyboard=True)
