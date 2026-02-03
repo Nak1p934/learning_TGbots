cd665b5cfaf82a21fd1bdc4d235e0977e68d8266
Nak1p934 <leonleon0308@gmail.com>
2026-02-03 14:06:15 +0300

    fix run file

--- lesson3/run3.py
+++ lesson3/run3.py
@@ -1,8 +1,17 @@
 from aiogram import Bot, Dispatcher
 import asyncio
 from BOTTOKEN_LESS3 import TOKEN_LESS3
+from routes_less3 import router
 
 async def main():
     bot = Bot(token=TOKEN_LESS3)
     dp = Dispatcher()
-    
+    dp.include_router(router=router)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except:
+        print("Завершение работы")
