ba9330310e14282c360cdfa07ccb283a38f64091
Nak1p934 <leonleon0308@gmail.com>
2026-02-03 14:22:03 +0300

    create new kb

--- lesson3/keyboards_less3.py
+++ lesson3/keyboards_less3.py
@@ -1,5 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
 
 main_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Да"), KeyboardButton(text="Нет")]
 ], resize_keyboard=True, input_field_placeholder="Ответь на вопрос кнопками", one_time_keyboard=True)
+
+async def coco():
+    coco_keyboard = ReplyKeyboardMarkup([
+        [KeyboardButton(text="Являюсь")],
+        [KeyboardButton(text="Нет😭")]
+    ], resize_keyboard=True, input_field_placeholder="Изменил надпись", one_time_keyboard=True)
--- lesson3/routes_less3.py
+++ lesson3/routes_less3.py
@@ -1,10 +1,15 @@
 from aiogram.types import ReplyKeyboardMarkup, keyboard_button, Message
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command
-from keyboards_less3 import main_kb
+from keyboards_less3 import main_kb, coco
 
 router = Router()
 
 @router.message(Command("test"))
 async def test_reply(message: Message):
     await message.answer(text="Вы старше 18 лет?", reply_markup=main_kb)
+
+
+@router.message(F.text == "Да")
+async def new_kb(message: Message):
+    await message.answer(text="Вы гражданин Японии?", reply_markup=coco())
